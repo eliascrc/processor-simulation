1fcb889e340943d92ecb44b14fecd4e11c73d91c
Josue Leon Sarkis <b53846@ecci.ucr.ac.cr>
2018-06-07 20:53:06 -0600

    Created the cache related classes and included its basic attributes and methods

---
+++ src/main/java/cr/ac/ucr/ecci/ci1323/cache/DataCache.java
@@ -0,0 +1,33 @@
+package main.java.cr.ac.ucr.ecci.ci1323.cache;
+
+/**
+ * Represents the position of a data cache, with its respective block and tag.
+ *
+ * @author Josué León Sarkis, Elías Calderón, Daniel Montes de Oca
+ */
+public class DataCache {
+
+    private DataCachePosition[] dataCachePositions;
+    private Bus dataBus;
+
+    public DataCache(DataCachePosition[] dataCachePositions, Bus dataBus) {
+        this.dataCachePositions = dataCachePositions;
+        this.dataBus = dataBus;
+    }
+
+    public DataCachePosition[] getDataCachePositions() {
+        return dataCachePositions;
+    }
+
+    public void setDataCachePositions(DataCachePosition[] dataCachePositions) {
+        this.dataCachePositions = dataCachePositions;
+    }
+
+    public Bus getDataBus() {
+        return dataBus;
+    }
+
+    public void setDataBus(Bus dataBus) {
+        this.dataBus = dataBus;
+    }
+}
--- src/main/java/cr/ac/ucr/ecci/ci1323/cache/DummyCache.java
+++
@@ -1,4 +0,0 @@
-package cr.ac.ucr.ecci.ci1323.cache;
-
-public class DummyCache {
-}
